09a2a4546d8afe4494f1d21a45170305cf64aca3
AkihiroYamamoto <akiyam0613@gmail.com>
2019-12-09 15:59:09 +0900

    プロジェクト作成

---
+++ Server/Application.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class Application
+
+    ''' <summary>処理開始</summary>
+    ''' <param name="e">引数</param>
+    Protected Overrides Sub OnStartup(e As StartupEventArgs)
+
+        MyBase.OnStartup(e)
+
+    End Sub
+
+End Class
