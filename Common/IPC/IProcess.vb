eb3bd118c222e4dc705a78421e6554f2ba547a67
AkihiroYamamoto <akiyam0613@gmail.com>
2019-12-11 09:22:07 +0900

    Commonプロジェクトの実装

---
+++ Common/IPC/IProcess.vb
@@ -0,0 +1,16 @@
+﻿Imports System.ServiceModel
+
+Namespace IPC
+
+    ''' <summary>プロセス間通信インターフェース</summary>
+    <ServiceContract>
+    Public Interface IProcess
+
+        ''' <summary>サーバー側でメソッドを実行し、戻り値をクライアントに渡す</summary>
+        ''' <param name="sec">処理秒数</param>
+        <OperationContract>
+        Function Execute(sec As Integer) As Integer
+
+    End Interface
+
+End Namespace
---
+++ Common/IPC/Service.vb
@@ -0,0 +1,23 @@
+﻿Namespace IPC
+
+    ''' <summary>IPC通信にて使用するメソッド用クラス</summary>
+    Public Module Service
+
+        ''' <summary>アドレス取得</summary>
+        Public Function GetAddress() As String
+            Return My.Resources.BaseAddress & "/" & My.Resources.Endpoint
+        End Function
+
+        ''' <summary>ベースアドレス取得</summary>
+        Public Function GetBaseAddress() As String
+            Return My.Resources.BaseAddress
+        End Function
+
+        ''' <summary>エンドポイント取得</summary>
+        Public Function GetEndpoint() As String
+            Return My.Resources.Endpoint
+        End Function
+
+    End Module
+
+End Namespace
